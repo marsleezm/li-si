12bdd0185526884a84b081a6a93e120efaf6876c
Zhongmiao Li <mars.leezm@gmail.com>
2015-06-14 12:00:30 +0100

    Change wait_inti

--- riak_test/wait_init.erl
+++ riak_test/wait_init.erl
@@ -15,16 +15,19 @@ wait_ready_nodes([Node|Rest]) ->
     end.
 
 check_ready(Node) ->
+    lager:info("Waiting for clocksi"),
     case rpc:call(Node,clocksi_vnode,check_tables_ready,[]) of
 	true ->
+        lager:info("Waiting for readitemfsm"),
 	    case rpc:call(Node,clocksi_readitem_fsm,check_servers_ready,[]) of
 		true ->
-		    case rpc:call(Node,materializer_vnode,check_tables_ready,[]) of
-			true ->
+                lager:info("Done"),
+		    %case rpc:call(Node,materializer_vnode,check_tables_ready,[]) of
+			%true ->
 			    true;
-			false ->
-			    false
-		    end;
+			%false ->
+			%    false
+		    %end;
 		false ->
 		    false
 	    end;
